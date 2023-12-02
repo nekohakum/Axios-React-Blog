5a6fa79b5b8255aeccf653bec2e9e51277016957
qw1216 <qiuwei1216@foxmail.com>
2023-12-02 20:18:31 +0800

    The blog was developed using axios and json-server.

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = ({ title }) => {
+      return (
+            <header className="Header">
+                  <h2>{title}</h2>
+            </header>
+      );
+};
+
+export default Header;
